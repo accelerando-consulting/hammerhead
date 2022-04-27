de2a23127d50059833ae090b5a7e012e33ee5682
Christopher Biggs <unixbigot@pobox.com>
2022-04-27 18:41:40 +1000

    Support for ESP32 and ESP32-c3. New leaves for dust and co2

--- Makefile
+++ Makefile
@@ -1,23 +1,44 @@
-BOARD ?= esp8266:esp8266:d1_mini_pro
-//CCFLAGS ?= --warnings default --verbose 
-//CPPFLAGS = -Istacx -I$(PWD)
-CPPFLAGS=-DST7735_DRIVER=1 \
-	-DTFT_WIDTH=128 \
-	-DTFT_HEIGHT=160 \
-	-DTFT_SCLK=14 \
-	-DTFT_MISO=12 \
-	-DTFTF_MOSI=13 \
-	-DTFT_CS=2 \
-	-DTFT_DC=0 \
-	-DTFT_RST=-1 \
-	-DLOAD_GLCD=1 \
+PARTITION_SCHEME=min_spiffs
+#BOARD ?= espressif:esp32:ttgo-t-oi-plus
+#BOARD ?= espressif:esp32:esp32c3
+BOARD ?= espressif:esp32:ttgo-t7-v13-mini32
+
+# Pins for ESP8266
+# TFTPINS=-DTFT_SCLK=14 \
+# 	-DTFT_MISO=12 \
+# 	-DTFTF_MOSI=13 \
+# 	-DTFT_CS=2 \
+# 	-DTFT_DC=0 \
+# 	-DTFT_RST=-1
+# Pins for ESP32-C3
+# TFTPINS=-DTFT_SCLK=5 \
+# 	-DTFT_MISO=6 \
+# 	-DTFTF_MOSI=7 \
+# 	-DTFT_CS=8 \
+# 	-DTFT_DC=9 \
+# 	-DTFT_RST=-1
+
+TFTPINS=-DTFT_SCLK=18 \
+	-DTFT_MISO=19 \
+	-DTFTF_MOSI=23 \
+	-DTFT_CS=16 \
+	-DTFT_DC=17 \
+	-DTFT_RST=-1
+
+TFTFONTS=-DLOAD_GLCD=1 \
 	-DLOAD_FONT2=1 \
 	-DLOAD_FONT4=1 \
 	-DLOAD_FONT6=1 \
 	-DLOAD_FONT7=1 \
 	-DLOAD_FONT8=1 \
 	-DLOAD_GFXFF=1 \
-	-DSMOOTH_FONT=1 \
+	-DSMOOTH_FONT=1 
+
+CPPFLAGS=-DST7735_DRIVER=1 \
+	-DTFT_WIDTH=128 \
+	-DTFT_HEIGHT=160 \
+	$(TFTPINS) \
+	$(TFTFONTS) \
 	-DSPI_FREQUENCY=27000000 \
 	-DUSER_SETUP_LOADED=1
 
--- hammerhead.ino
+++ hammerhead.ino
@@ -1,32 +1,48 @@
 #include "config.h"
 #include "stacx/stacx.h"
 
+#ifdef ESP8266
 #include "stacx/leaf_fs_preferences.h"
+#else
+#include "stacx/leaf_preferences.h"
+#endif
 #include "stacx/leaf_ip_esp.h"
 #include "stacx/leaf_pubsub_mqtt_esp.h"
 #include "stacx/leaf_wire.h"
 #include "stacx/leaf_dht12.h"
 #include "stacx/leaf_shell.h"
 #include "stacx/leaf_rtc.h"
-//#include "stacx/leaf_sdcard.h"
-#include "stacx/leaf_max44009.h"
 #include "stacx/leaf_tft.h"
+#include "stacx/leaf_sdcard.h"
+#include "stacx/leaf_max44009.h"
+#include "stacx/leaf_sds011.h"
+#include "stacx/leaf_pwmsensor.h"
 #include "app_hammerhead.h"
 
-// default arrangment for an ESP8266 D1 Mini 1.4" TFT
+// Pins for TTGO T-OI ESP32-C3
 Leaf *leaves[] = {
+#ifdef ESP8266
 	new FSPreferencesLeaf("prefs"),
+#else
+	new PreferencesLeaf("prefs"),
+#endif
 	new ShellLeaf("shell"),
-	new IpEspLeaf("esp"),
-	new PubsubEspAsyncMQTTLeaf("espmqtt"),
-	new TFTLeaf("screen",1), 
+	new IpEspLeaf("esp","",false),
+	new PubsubEspAsyncMQTTLeaf("espmqtt","",false,true,false),
+	new TFTLeaf("screen",1),  // see Makefile for pin defines
 	//new SDCardLeaf("sdcard"), 
+	// for C3
+	//new PWMSensorLeaf("co2",LEAF_PIN(2)), 
+	//new SDS011SensorLeaf("dust",LEAF_PIN(4)), 
+	// for ESP32
+	new PWMSensorLeaf("co2",LEAF_PIN(36)), 
+	new SDS011Leaf("dust",2,9600,SERIAL_8N1,26,25), 
 
 	new WireBusLeaf("wire"),
 	new RTCLeaf("rtc"),
 	new Dht12Leaf("dht"),
 	new MAX44009Leaf("lux"),
-	new HammerheadAppLeaf("hammerhead", "screen,dht,lux,sdcard"),
+	new HammerheadAppLeaf("hammerhead", "screen,dht,lux,co2,dust,sdcard"),
 	NULL
 };
 
